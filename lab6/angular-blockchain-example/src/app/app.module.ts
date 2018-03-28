c0b5c48a1a39b892a7a0e0abe5caa17dd2240d0a
Liad Ginosar <liadginosar@gmail.com>
2018-03-28 22:30:08 +0300

    accounts and first-contract pages

--- lab6/angular-blockchain-example/src/app/app.module.ts
+++ lab6/angular-blockchain-example/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { FirstContractComponent } from './first-contract/first-contract.component';
+import { Web3Service } from './web3.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountsComponent,
+    FirstContractComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [Web3Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
